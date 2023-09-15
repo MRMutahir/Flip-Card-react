5790aff543e4daefb4ed4bcaf2e86061a1a90458
MRMutahir <mutahirkaeem820@gmail.com>
2023-09-15 05:27:02 -0700

    bad req

--- src/Components/Main.jsx/Main.jsx
+++ src/Components/Main.jsx/Main.jsx
@@ -6,35 +6,47 @@ import Card from 'react-bootstrap/Card';
 import "./Main.css"
 function Main() {
     const [movedata, setmovedata] = useState(MOCK);
-    function movefoo(indexٖ) {
-        const restdata = movedata[indexٖ]
-        // setmovedata(restdata[indexٖ])
-        console.log(restdata);
-        // setmovedata(restdata)
+    function movefoo(index) {
+        let newArr = [...movedata]
+        newArr[index].first_name = movedata[index].email
+        // console.log(setmovedata);
+        setmovedata(newArr)
+        console.log(newArr[index].first_name);
+    }
 
-        console.log("SALAM", indexٖ);
+    function moveleave(index) {
+        // setmovedata()
+        let newArr = [...MOCK]
+        console.log(newArr);
+        console.log(MOCK);
+        // newArr[index].email = movedata[index].first_name
+        // setmovedata(newArr)
+        // console.log(newArr[index].email);
+        // console.log(movedata[index].first_name);
+
     }
-    function moveleave(indexٖ) {
-        // setmovedata(Students)
-        console.log("SALAM moveleave", indexٖ);
-    }
+
+
     return (
         <div className="card-parent">
-            {movedata.map((ele, indexٖ) => {
-                return (<Card style={{ width: '18rem' }} className='card-parent'>
-                    <Card.Body
-                        className='card-main'
-                        onMouseLeave={() => moveleave(indexٖ)}
-                        onMouseOver={() => movefoo(indexٖ)}>
-                        <Card.Img variant="top" src={ele.id ? ele.id : ""} style={{ borderRadius: "50%" }} />
-                        <Card.Title>{ele.first_name ? ele.first_name : ""}</Card.Title>
-                        <Card.Text>
-                            {ele.email ? ele.email : ""}
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>)
-            })}
+            {movedata.map
+                ((ele, indexٖ) => {
+                    return (
+                        <Card style={{ width: '18rem' }} className='card-parent'>
+                            <Card.Body
+                                className='card-main'
+
+                                onMouseLeave={() => moveleave(indexٖ)}
+                                onMouseOver={() => movefoo(indexٖ)}>
+                                {/* <Card.Img variant="top" src={ele.id ? ele.id : ""} style={{ borderRadius: "50%" }} /> */}
+                                <Card.Title>{ele.first_name}</Card.Title>
+                                <Card.Text>
+                                    {/* {ele.email} */}
+                                </Card.Text>
+
+                            </Card.Body>
+                        </Card>)
+                })}
 
 
         </div>
