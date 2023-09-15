aa4adbf8368257c7aae1e60ba52e472944d41bc5
MRMutahir <mutahirkaeem820@gmail.com>
2023-09-15 06:42:15 -0700

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Footer from "../src/Footer"
 import './App.css'
 import Main from './Components/Main.jsx/Main'
 function App() {
@@ -8,7 +7,13 @@ function App() {
 
   return (
     <>
+      <center className='center'><button className="btn">
+        <span className="loading loading-spinner"></span>
+        Fake
+        email
+      </button></center>
       <Main />
+      <Footer />
     </>
   )
 }
--- src/Components/Main.jsx/Main.jsx
+++ src/Components/Main.jsx/Main.jsx
@@ -1,28 +1,40 @@
 import React, { useState } from 'react'
 import MOCK from "../../MOCK_DATA (1).json"
-import Students from "../../Que.json"
+// import Students from "../../Que.json"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import "./Main.css"
 function Main() {
-    const [movedata, setmovedata] = useState(MOCK);
+    
+
+    let moakdata = MOCK
+    const [movedata, setmovedata] = useState(moakdata);
     function movefoo(index) {
         let newArr = [...movedata]
         newArr[index].first_name = movedata[index].email
-        // console.log(setmovedata);
+        console.log(newArr);
         setmovedata(newArr)
-        console.log(newArr[index].first_name);
+        // console.log(newArr[index].first_name);
     }
 
     function moveleave(index) {
-        // setmovedata()
-        let newArr = [...MOCK]
+        // setmovedata(MOCK)
+        let newArr = [...movedata]
+
+        newArr[index].first_name = "email"
+         // newArr[index].first_name = MOCK[index].email
+
         console.log(newArr);
-        console.log(MOCK);
-        // newArr[index].email = movedata[index].first_name
-        // setmovedata(newArr)
-        // console.log(newArr[index].email);
-        // console.log(movedata[index].first_name);
+        setmovedata(newArr)
+
+        // // newArr[index].email = movedata[index].first_name
+        // console.log(newArr);
+        // // console.log(newArr);
+        // // console.log(MOCK);ٖ
+        // // newArr[index].email = movedata[index].first_name
+        // // setmovedata(newArr)
+        // // console.log(newArr[index].email);
+        // // console.log(movedata[index].first_name);
 
     }
 
